06b932ddc22345b4626fc9461dd086dcad21589a
alexey <alex_kobilianskiy@bigmir.net>
2019-01-26 11:45:27 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+
+        });
+        return false;
+    });
+});
