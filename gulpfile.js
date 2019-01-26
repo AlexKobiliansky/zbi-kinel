6a41bcfa35280d2f3730ab3387708ae48b8e5c74
alexey <alex_kobilianskiy@bigmir.net>
2019-01-26 23:55:20 +0200

    section partners complete

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('rsync', function() {
 	.pipe(rsync({
 		root: 'app/',
 		hostname: 'alexko89@alexko89.beget.tech',
-		destination: 'demos.alexko89.beget.tech/public_html/dirname/',
+		destination: 'demos.alexko89.beget.tech/public_html/beton/',
 		// include: ['*.htaccess'], // Includes files to deploy
 		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
 		recursive: true,
